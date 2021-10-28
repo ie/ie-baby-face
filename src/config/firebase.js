c4fb7777c2b9a3597189f83c7801c7d333749bf7
Tim Burch <tim.burch@users.noreply.github.com>
2021-10-28 14:12:36 +1100

    hopefully fix the reference

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
-import 'firebase/firestore'
+import '@firebase/firestore'
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from "@firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,7 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore()
 
