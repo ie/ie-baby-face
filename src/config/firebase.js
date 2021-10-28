071d750f8bfccd7f7c9a475fbcd766a64ac1d163
Tim Burch <tim.burch@users.noreply.github.com>
2021-10-28 13:43:42 +1100

    more firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
-import '@firebase/firestore'
+import 'firebase/firestore'
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "@firebase/app";
+import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,6 +12,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCO9w27NALvv2a28ue61FHEtqMlN94kor8",
   authDomain: "baby-face-2021.firebaseapp.com",
   projectId: "baby-face-2021",
+  databaseURL: 'https://baby-face-2021.firebaseio.com',
   storageBucket: "baby-face-2021.appspot.com",
   messagingSenderId: "966933873357",
   appId: "1:966933873357:web:c5d42e903e17e804e35f84"
@@ -19,7 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 export const db = firebase.firestore()
 
